8bb2c614c6e20a3620ca429fe2955648a9f04e41
Anxhela <118725186+AnxhelaAhmetaj@users.noreply.github.com>
2022-11-29 19:36:56 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import fsp from 'fs/promises';
+import sumOfNumbers from './sumOfNumbers.js';
+import findUniqueStrings from './uniqueStrings.js';
+
+try {
+    const content = await fsp.readFile('./data.csv', 'utf-8');
+
+    //here we check if the content cointains whitespace or digits
+    if(/(\s)*(\d)/g.test(content)) {
+        await sumOfNumbers(content);
+    }
+    else{
+        await findUniqueStrings(content); 
+    } 
+}
+catch (error) {
+    console.log(error.message); 
+}
